acf53aaec3f7366e20d29b41f956fdc974d913ac
aicioara <primulandrei@yahoo.com>
2014-10-28 19:53:50 +0000

    Placeholder for TSP ready

--- algorithm/tsp.cpp
+++ algorithm/tsp.cpp
@@ -26,7 +26,56 @@ ofstream g(string (string(FILE) + ".out").c_str());
 #define g cout
 #endif
 
+/**
+ * Input structure:
+ *
+ * First line, 2 space separated integers, n, m
+ * n - the number of nodes
+ * m - the number of known distances
+ *
+ * Starting node is indexed 0, all other nodes are contiguous integers greater
+ * than 0.
+ *
+ * m lines follow having 3 integers per line x, y, c
+ * x - start node
+ * y - destination node
+ * c - cost associated to travelling from x to y
+ *
+ * !! It cannot be assumed that if x->y has cost c than y->x also has cost c
+ * The graph is directed.
+ *
+ */
+
+
+/**
+ * At the moment the algorithm is returning
+ */
+
+struct Edge {
+	int cost;
+	int node;
+};
+
+const int NMAX = 1000;
+int n, m, x, y, c;
+vector<Edge> distances[NMAX];
 
 int main() {
-	g << "Hello World" << e;
+
+	f >> n >> m;
+
+	for (int i = 0; i <= n; ++i) {
+		f >> x >> y >> c;
+		Edge * edge = new Edge();
+		edge -> cost = c;
+		edge -> node = y;
+		distances[x].push_back(*edge);
+	}
+
+	for (int i = 0; i <= n; ++i) {
+		g << i << " ";
+	}
+
+	g << 0 << e;
+
 }
