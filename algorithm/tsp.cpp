51b036231efca80ba66a30edcd19e3aa77585de9
aicioara <primulandrei@yahoo.com>
2014-10-28 19:39:07 +0000

    Skelleton for the TSP placeholder. Makefile included.

---
+++ algorithm/tsp.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <bitset>
+#include <list>
+
+using namespace std;
+
+#define e '\n'
+#define INF 1023456789
+#define ll long long
+
+//#define FILE "data"
+
+#ifdef FILE
+ifstream f(string (string(FILE) + ".in").c_str());
+ofstream g(string (string(FILE) + ".out").c_str());
+#endif
+#ifndef FILE
+#define f cin
+#define g cout
+#endif
+
+
+int main() {
+	g << "Hello World" << e;
+}
