a5cdb7e60952ce7bceb37d2429dc0f03ab1a0236
Andrea Michi <andrea.michi12@imperial.ac.uk>
2014-11-09 17:45:35 +0000

    Application directory restructure

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./app/routes/index');
+var users = require('./app/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'web/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -56,5 +56,4 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
-module.exports = app;
+app.listen(3000);
--- frontend/README.md
+++
@@ -1,6 +0,0 @@
-FrontEnd
-========
-
-This is the independent frontend, which might or might not be backed by some service (NodeJS).
-
-This displays the map to the user and talks to our API (see folder api)
