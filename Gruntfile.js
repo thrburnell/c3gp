0f2bd2d386d85a9343993e8a61235b211994de84
Jeremy Kong <jeremykong91@gmail.com>
2014-11-10 23:23:45 +0000

    Set up Grunt to minify stylesheets, and adjusted gitignore/dependency list accordingly

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+  
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['web/javascripts/*.js'],
+        dest: 'public/js/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/* <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'public/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+
+    cssmin: {
+      options: {
+        banner: '/* <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      minify: {
+        expand: true,
+        cwd: 'web/stylesheets/',
+        src: ['*.css'],
+        dest: 'public/css/',
+        ext: '.min.css'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('static', ['concat', 'uglify', 'cssmin']);
+};
+
