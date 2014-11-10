be2478450bb357825ecb99324fba95e8572a8205
Jeremy Kong <jeremykong@hotmail.com>
2014-11-10 15:18:25 +0000

    Implemented functionality to add and delete markers from the map, storing them in JS array

--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,34 @@
+var markers = [];
+
 function initialize() {
   var mapOptions = {
     center: {lat: 51.498800, lng: -0.174877},
     zoom: 16
   };
+
   var map = new google.maps.Map(document.getElementById('map-canvas'),
                                 mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
 
+  google.maps.event.addListener(map, 'click', function(event) {
+    var marker = new google.maps.Marker({
+      position: event.latLng,
+      map: map
+    });
+
+    // TODO Eventually we should change this to push just the position.
+    markers.push(marker);
+    console.log(markers);
+
+    google.maps.event.addListener(marker, 'click', function(point) {
+      marker.setMap(null); // removes the marker from the map
+      for(var i = 0; i < markers.length; ++i) {
+        if(markers[i] === marker) {
+          markers.splice(i, 1); // removes marker from array of markers
+        }
+      }
+      console.log(markers);
+    });
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
