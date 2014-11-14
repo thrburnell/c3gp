8e95b1aceb0fc050e1008860bcb4a40e4412fa97
aicioara <primulandrei@yahoo.com>
2014-11-14 14:29:47 +0000

    Json encoding done

---
+++ algorithm/src/jsonParsingTest.cpp
@@ -0,0 +1,107 @@
+// // rapidjson/example/simpledom/simpledom.cpp`
+// #include "lib/rapidjson/document.h"
+// #include "lib/rapidjson/writer.h"
+// #include "lib/rapidjson/stringbuffer.h"
+// #include <iostream>
+// #include <string>
+// #include <cstring>
+// #include <vector>
+// #include <memory>
+
+// using namespace rapidjson;
+// using namespace std;
+
+
+// struct Coordinate {
+//     double lat;
+//     double lng;
+// };
+
+// struct MapPoints {
+//     unique_ptr<Coordinate> origin;
+//     unique_ptr<Coordinate> destination;
+//     unique_ptr<vector<unique_ptr<Coordinate>>> errands;
+// };
+
+// unique_ptr<MapPoints> parse_coordinates(char * json) {
+//     Document document;
+//     document.Parse(json);
+
+//     unique_ptr<MapPoints> mapPoints(new MapPoints);
+
+//     unique_ptr<Coordinate> origin(new Coordinate);
+//     origin->lat = document["origin"]["lat"].GetDouble();
+//     origin->lng = document["origin"]["lng"].GetDouble();
+
+//     unique_ptr<Coordinate> destination(new Coordinate);
+//     destination->lat = document["destination"]["lat"].GetDouble();
+//     destination->lng = document["destination"]["lng"].GetDouble();
+
+//     unique_ptr<vector<unique_ptr<Coordinate>>> errands(new vector<unique_ptr<Coordinate>>);
+
+//     const Value & waypoints = document["waypoints"];
+//     for (int i = 0; i < waypoints.Size(); i++) {
+//         unique_ptr<Coordinate> newCoordinate(new Coordinate);
+//         newCoordinate->lat = waypoints[i]["lat"].GetDouble();
+//         newCoordinate->lng = waypoints[i]["lng"].GetDouble();
+//         errands->push_back(move(newCoordinate));
+//     }
+
+//     mapPoints->origin = move(origin);
+//     mapPoints->destination = move(destination);
+//     mapPoints->errands = move(errands);
+
+//     return mapPoints;
+// }
+
+
+// unique_ptr<MapPoints> process_coordinates(unique_ptr<MapPoints> map_points) {
+
+//     return move(map_points);
+
+// }
+
+// void print_coordinates(unique_ptr<MapPoints> map_points) {
+
+//     StringBuffer s;
+//     Writer<StringBuffer> writer(s);
+
+//     writer.StartArray();
+
+//     for (auto it = map_points->errands->begin(); it != map_points->errands->end(); it++) {
+//         writer.StartObject();
+
+//         writer.String("lat");
+//         writer.Double((*it)->lat);
+
+
+//         writer.String("lng");
+//         writer.Double((*it)->lng);
+
+//         writer.EndObject();
+//     }
+
+//     writer.EndArray();
+
+//     cout << s.GetString() << endl;
+// }
+
+
+
+// int main() {
+
+//     // Weird way of getting a file into a char*
+//     string ss;
+//     getline(cin, ss);
+//     char * json = (char*) ss.c_str();
+
+//     unique_ptr<MapPoints> mp = parse_coordinates(json);
+//     mp = process_coordinates(move(mp));
+//     print_coordinates(move(mp));
+
+
+//     return EXIT_SUCCESS;
+// }
+
+
+
--- algorithm/src/tsp.cpp
+++ algorithm/src/tsp.cpp
@@ -1,62 +1,107 @@
+// rapidjson/example/simpledom/simpledom.cpp`
+#include "lib/rapidjson/document.h"
+#include "lib/rapidjson/writer.h"
+#include "lib/rapidjson/stringbuffer.h"
 #include <iostream>
+#include <string>
+#include <cstring>
 #include <vector>
 #include <memory>
 
+using namespace rapidjson;
 using namespace std;
 
-/**
- * Input structure:
- *
- * First line, 2 space separated integers, n, m
- * n - the number of nodes
- * m - the number of known distances
- *
- * Starting node is indexed 0, all other nodes are contiguous integers greater
- * than 0.
- *
- * m lines follow having 3 integers per line x, y, c
- * x - start node
- * y - destination node
- * c - cost associated to travelling from x to y
- *
- * !! It cannot be assumed that if x->y has cost c than y->x also has cost c
- * The graph is directed.
- *
- */
 
-
-/**
- * At the moment the algorithm is returning the identity permutation
- */
-
-struct Edge {
-	int cost;
-	int node;
+struct Coordinate {
+    double lat;
+    double lng;
 };
 
-const int NMAX = 1000;
+struct MapPoints {
+    unique_ptr<Coordinate> origin;
+    unique_ptr<Coordinate> destination;
+    unique_ptr<vector<unique_ptr<Coordinate>>> errands;
+};
 
-int main2() {
+unique_ptr<MapPoints> parse_coordinates(char * json) {
+    Document document;
+    document.Parse(json);
 
-	int n, m, x, y, c;
-	vector<unique_ptr<Edge>> distances[NMAX];
+    unique_ptr<MapPoints> mapPoints(new MapPoints);
 
-	cin >> n >> m;
+    unique_ptr<Coordinate> origin(new Coordinate);
+    origin->lat = document["origin"]["lat"].GetDouble();
+    origin->lng = document["origin"]["lng"].GetDouble();
 
-	for (int i = 0; i < n + 1; ++i) {
-		cin >> x >> y >> c;
-		unique_ptr<Edge> edge(new Edge);
-		edge->cost = c;
-		edge->node = y;
-		distances[x].push_back(move(edge));
-	}
+    unique_ptr<Coordinate> destination(new Coordinate);
+    destination->lat = document["destination"]["lat"].GetDouble();
+    destination->lng = document["destination"]["lng"].GetDouble();
 
-	for (int i = 0; i <= n; ++i) {
-		cout << i << " ";
-	}
+    unique_ptr<vector<unique_ptr<Coordinate>>> errands(new vector<unique_ptr<Coordinate>>);
 
-	cout << 0 << endl;
+    const Value & waypoints = document["waypoints"];
+    for (int i = 0; i < waypoints.Size(); i++) {
+        unique_ptr<Coordinate> newCoordinate(new Coordinate);
+        newCoordinate->lat = waypoints[i]["lat"].GetDouble();
+        newCoordinate->lng = waypoints[i]["lng"].GetDouble();
+        errands->push_back(move(newCoordinate));
+    }
 
-	return 0;
+    mapPoints->origin = move(origin);
+    mapPoints->destination = move(destination);
+    mapPoints->errands = move(errands);
+
+    return mapPoints;
+}
+
+
+unique_ptr<MapPoints> process_coordinates(unique_ptr<MapPoints> map_points) {
+
+    return move(map_points);
 
 }
+
+void print_coordinates(unique_ptr<MapPoints> map_points) {
+
+    StringBuffer s;
+    Writer<StringBuffer> writer(s);
+
+    writer.StartArray();
+
+    for (auto it = map_points->errands->begin(); it != map_points->errands->end(); it++) {
+        writer.StartObject();
+
+        writer.String("lat");
+        writer.Double((*it)->lat);
+
+
+        writer.String("lng");
+        writer.Double((*it)->lng);
+
+        writer.EndObject();
+    }
+
+    writer.EndArray();
+
+    cout << s.GetString() << endl;
+}
+
+
+
+int main() {
+
+    // Weird way of getting a file into a char*
+    string ss;
+    getline(cin, ss);
+    char * json = (char*) ss.c_str();
+
+    unique_ptr<MapPoints> mp = parse_coordinates(json);
+    mp = process_coordinates(move(mp));
+    print_coordinates(move(mp));
+
+
+    return EXIT_SUCCESS;
+}
+
+
+
