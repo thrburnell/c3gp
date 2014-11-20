9f357d69df1d35374a5f60eff22c6241935f4954
aicioara <primulandrei@yahoo.com>
2014-11-20 13:24:25 +0000

    Added heuristic to always go to the closest point first. Euclidean distance used
    hacky thing. Now coordinates are shown in correct order
    
    Refactor dead code
    
    Extra newline
    
    Added TODO
    
    Integrated CR suggestions

--- algorithm/src/map_points.cpp
+++ algorithm/src/map_points.cpp
@@ -3,8 +3,10 @@
 #include "lib/rapidjson/stringbuffer.h"
 
 #include <iostream>
+#include <limits>
 
 #include "map_points.h"
+#include "haversine.h"
 
 namespace rj = rapidjson;
 using std::unique_ptr;
@@ -44,13 +46,74 @@ unique_ptr<MapPoints> parse_coordinates(const char * const json) {
 }
 
 
+//TODO: Use unique_ptr properly in this function
 /**
- * TODO: We should 1) Move this to a process file 2) implement
  * @param The given data points
  * @return Some processing on this data (possibly)
  */
 unique_ptr<MapPoints> process_coordinates(unique_ptr<MapPoints> map_points) {
-    return move(map_points);
+
+    unique_ptr<std::vector<unique_ptr<Coordinate>>> r_errands(new std::vector<unique_ptr<Coordinate>>);
+
+    std::vector<Coordinate *> v;
+
+    Coordinate * origin = new Coordinate();
+    origin->lat = map_points->origin->lat;
+    origin->lng = map_points->origin->lng;
+
+    Coordinate * destination = new Coordinate();
+    destination->lat = map_points->destination->lat;
+    destination->lng = map_points->destination->lng;
+
+    for (
+            std::vector<unique_ptr<Coordinate>>::iterator it = map_points->errands->begin();
+            it != map_points->errands->end();
+            it++) {
+        Coordinate * newCoord = new Coordinate();
+        newCoord->lat = (*it)->lat;
+        newCoord->lng = (*it)->lng;
+
+        v.push_back(newCoord);
+    }
+
+    Coordinate * iterator = origin;
+
+    while (! v.empty()) {
+        double min_dist = std::numeric_limits<double>::max();
+        std::vector<Coordinate *>::iterator min_dist_iterator;
+        for (std::vector<Coordinate *>::iterator it = v.begin(); it != v.end(); it++) {
+            double curr_dist = calculate_distance((const Coordinate&) (*iterator), (const Coordinate&) (*(*it)));
+            if (curr_dist < min_dist) {
+                min_dist = curr_dist;
+                min_dist_iterator = it;
+            }
+        }
+
+        unique_ptr<Coordinate> temp_coordinate(new Coordinate);
+        temp_coordinate->lat = (*min_dist_iterator)->lat;
+        temp_coordinate->lng = (*min_dist_iterator)->lng;
+
+        r_errands->push_back(move(temp_coordinate));
+
+        iterator = *min_dist_iterator;
+        v.erase(min_dist_iterator);
+    }
+
+    unique_ptr<MapPoints> result(new MapPoints);
+
+    unique_ptr<Coordinate> r_origin(new Coordinate);
+    r_origin->lat = origin->lat;
+    r_origin->lng = origin->lng;
+
+    unique_ptr<Coordinate> r_destination(new Coordinate);
+    r_destination->lat = destination->lat;
+    r_destination->lat = destination->lng;
+
+    result->origin = move(r_origin);
+    result->destination = move(r_destination);
+    result->errands = move(r_errands);
+
+    return move(result);
 }
 
 
