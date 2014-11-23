7c502d269a5977f69225ce957e7fde918b38ea86
aicioara <primulandrei@yahoo.com>
2014-11-23 17:27:11 +0000

    Added unit tests for the routes

---
+++ tests/math.spec.js
@@ -0,0 +1,9 @@
+describe("My First Test", function() {
+	it("should be equal", function() {
+		expect(1 + 2).toEqual(3);
+	});
+
+	it("should not be equal", function() {
+		expect(4 + 3).toEqual(7);
+	});
+});
---
+++ tests/routes.spec.js
@@ -0,0 +1,33 @@
+var request = require('supertest');
+var express = require('express');
+var app = require('../app');
+
+request = request(app);
+
+describe("Accessibility to all routes", function() {
+
+    it("should have index", function(done) {
+        request
+        .get('/')
+        .expect(200)
+        .end(function(err, res) {
+            expect(err).toEqual(null);
+            done();
+        });
+    });
+
+    it("should have the cpp page", function(done) {
+        var data = {"origin":{"lat":51.499633334377386,"lng":-0.1795792579650879},"destination":{"lat":51.499633334377386,"lng":-0.1795792579650879},"waypoints":[{"lat":51.49999399415595,"lng":-0.17475128173828125},{"lat":51.49887193213894,"lng":-0.17691850662231445}]};
+        data = JSON.stringify(data);
+
+        request
+        .post('/cpp')
+        .send(data)
+        .expect(200)
+        .end(function(err) {
+            expect(err).toEqual(null);
+            done();
+        });
+    });
+
+});
