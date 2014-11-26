8be75672d3dcae2722411e0864f53fca6b8de596
Andrea Michi <andrea.michi12@imperial.ac.uk>
2014-11-26 14:17:41 +0000

    Completed task on grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,7 +90,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-copy');
-
   grunt.loadNpmTasks('grunt-jsonlint');
   grunt.loadNpmTasks('grunt-sass');
 
