7fa88133cf3f0c9ca356de5d2ea1d6554b801a6d
aicioara <primulandrei@yahoo.com>
2014-11-27 01:16:07 +0000

    Added test for new route

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -16,6 +16,17 @@ describe("Accessibility to all routes", function() {
         });
     }, 1000);
 
+    it("should have status page", function(done) {
+        request
+        .get('/status')
+        .expect(200)
+        .end(function(err, res) {
+            expect(err).toBe(null);
+            done();
+        });
+    }, 1000);
+
+
     it("should have the cpp page", function(done) {
         var data = {"origin":{"lat":51.499633334377386,"lng":-0.1795792579650879},"destination":{"lat":51.499633334377386,"lng":-0.1795792579650879},"waypoints":[{"lat":51.49999399415595,"lng":-0.17475128173828125},{"lat":51.49887193213894,"lng":-0.17691850662231445}]};
         data = JSON.stringify(data);
