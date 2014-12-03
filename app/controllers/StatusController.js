dde6c3ae542f78be627be29c12cbd2c814cd0bd9
aicioara <primulandrei@yahoo.com>
2014-12-03 18:43:43 +0000

    Added CR sugestions

--- app/controllers/StatusController.js
+++ app/controllers/StatusController.js
@@ -2,9 +2,9 @@ var exec = require('child_process').exec;
 
 exports.controller = function(req, res) {
 
-    var cppCommand = 'git log -1';
+    var gitCommand = 'git log -1';
 
-    var child = exec(cppCommand, function (error, stdout, stderr) {
+    var child = exec(gitCommand, function (error, stdout, stderr) {
         if (error !== null) {
             console.log('exec error: ' + error);
             res.status(500).end();
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -6,8 +6,8 @@ var IndexController = require('../controllers/IndexController');
 var StatusController = require('../controllers/StatusController');
 
 router.get('/', IndexController.controller);
+router.get('/status', StatusController.controller);
 
 router.post('/cpp', CppController.controller);
-router.get('/status', StatusController.controller);
 
 module.exports = router;
