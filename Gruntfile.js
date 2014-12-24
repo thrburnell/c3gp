ef493623e219da0758b37df4a3d9cc9e59639059
Tom Burnell <thrburnell@gmail.com>
2014-12-24 23:51:54 +0000

    Adds all required tasks to the Gruntfile.
    * Gruntfile now has all tasks so we never need to directly run scripts
      hidden throughout our project.
    * Run `grunt dev` in development to build and start watching server.
    * Run `grunt prepare` and then `grunt forever-start` on production
      environments.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,44 @@
+require('shelljs/global');
+
+// We use Grunt as a task runner. All tasks, such as compilation or test 
+// running, should be achieved by running one of these tasks.
+//
+// For a list of available tasks, run `grunt -h`. For example, running 
+// `grunt dev` compiles all code, moves all public files into place, 
+// starts the server, and watches for all file changes (restarting the
+// server when necessary, and moving public files across when modified).
+//
+// Additionally, all our production and demo environments run `grunt prepare`
+// before `grunt forever-start`. The first compiles all code and moves 
+// everything into place for server start, and the second starts/restarts
+// the server process. If additional jobs need to be carried out before 
+// starting the server, one should add the necessary code to the 'prepare'
+// task.
+//
+// During development, developers will often want to make use of `grunt dev`.
+// This task moves all public files into place, compiles all server code, and 
+// starts the server, after which making sure all files are watched for 
+// changes.
+
 module.exports = function(grunt) {
+
+  // Configuration for grunt tasks
   grunt.initConfig({
+   
+    // Location of server script
+    serverScript: 'bin/www',
+
+    // Load global project config
     pkg: grunt.file.readJSON('package.json'),
 
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['web/javascripts/*.js'],
-        dest: 'public/js/<%= pkg.name %>.js'
-      }
-    },
-
-    uglify: {
-      options: {
-        banner: '/* <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'public/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
-
-    cssmin: {
-      options: {
-        banner: '/* <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      minify: {
-        expand: true,
-        cwd: 'web/stylesheets/',
-        src: ['*.css'],
-        dest: 'public/css/',
-        ext: '.min.css'
-      }
-    },
-
-    sass: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'web/stylesheets',
-          src: ['*.scss'],
-          dest: 'public/css',
-          ext: '.css'
-        }]
-      }
-    },
-
-    copy: {
-      clientjs: {
-        expand: true,
-        cwd: 'web/javascripts',
-        src: '*.js',
-        dest: 'public/js',
-      }
-    },
-
-    jshint: {
-      all: ['*.js', 'web/javascripts/**/*.js', 'app/**/*.js']
-    },
-
-    jsonlint: {
-      jsonlint: {
-        src: ['*.json']
-      }
-    },
-
+    // Config for nodemon server
     nodemon: {
       dev: {
         script: 'bin/www'
       }
     },
 
+    // Config for running nodemon and watch tasks concurrently
     concurrent: {
       target: {
         tasks: ['nodemon', 'watch'],
@@ -82,6 +48,7 @@ module.exports = function(grunt) {
       }
     },
 
+    // Configure tasks to run on watched file changes
     watch: {
       sass: {
         files: ['web/stylesheets/*.scss'],
@@ -95,25 +62,161 @@ module.exports = function(grunt) {
         files: ['*.json'],
         tasks: ['jsonlint']
       },
-      // This task is temporary until we start minifying-concat the js
-      clientjs: {
+      js: {
         files: ['web/javascripts/**/*.js'],
-        tasks: ['copy']
+        tasks: ['copy:js']
+      },
+      css: {
+        files: ['web/stylesheets/**/*.css'],
+        tasks: ['copy:css']
       }
+    },
+
+    // Config for copying js/css files from web to public
+    copy: {
+      js: {
+        expand: true,
+        cwd: 'web/javascripts',
+        src: '*.js',
+        dest: 'public/js'
+      },
+      css: {
+        expand: true,
+        cwd: 'web/stylesheets',
+        src: '*.css',
+        dest: 'public/css'
+      }
+    },
+
+    // Config for compiling sass from web/stylesheets to public/css
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'web/stylesheets',
+          src: ['*.scss'],
+          dest: 'public/css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    // Config for hinting all js files
+    jshint: {
+      all: ['*.js', 'web/javascripts/**/*.js', 'app/**/*.js']
+    },
+
+    // Config for linting all json files
+    jsonlint: {
+      jsonlint: {
+        src: ['*.json']
+      }
+    },
+
+    // Config specifying location of test run scripts
+    test: {
+      algorithm: {
+        dir: 'algorithm',
+        script: 'runTests.py'
+      },
+
+      server: {
+        dir: '.',
+        script: 'runTests.py'
+      }
+    },
+
+    // Config specifying tasks to run for validating different file types
+    validate: {
+      json: 'jsonlint',
+      js: 'jshint'
     }
+
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('algorithm', 
+      'Installs all libraries and compiles algorithm code.',
+      function() {
+        // Execute install script in algorithm dir
+        exec('(cd algorithm; ./install.py)');
+      });
+
+  // grunt test:{algorithm / server / front}
+  grunt.registerMultiTask('test',
+      'Runs (optionally specified) test suites.',
+      function() {
+        // Run test script for specified target
+        exec('(cd ' + this.data.dir + '; ./' + this.data.script + ')');
+      });
+
+  grunt.registerTask('public',
+      'Generates public static files.',
+      ['copy']);
+
+  grunt.registerTask('dev',
+      'Compiles all code, starts server, watches for all file changes.',
+      ['prepare', 'concurrent']);
+
+  grunt.registerTask('prepare',
+      'Runs all tasks (e.g. compilation) needed to start the server.',
+      function() {
+
+        // If additional tasks need to be run before the server can be 
+        // started, they should be implemented here
+
+        // npm install
+        if (exec('npm install').code !== 0) {
+          grunt.log.writeln('Failed to install npm modules.');
+        }
+
+        // bower install
+        if (exec('bower install').code !== 0) {
+          grunt.log.writeln('Failed to install bower components.');
+        }
+
+        // Compile all algorithm code and generate public files
+        grunt.task.run(['algorithm', 'public']);
+
+      });
+
+  grunt.registerTask('forever-start',
+      'Starts/restarts a forever process to run the server.',
+      function() {
+
+        script = grunt.config('serverScript');
+
+        // Attempt to restart process
+        if (exec('forever restart ' + script).code !== 0) {
+          
+          // Restart failed (i.e. it was never started to begin with)
+          if (exec('forever start ' + script).code !== 0) {
+            // Start failed unexpectedly
+            grunt.log.writeln('Failed to start forever process.');
+          }
+        }
+
+      });
+
+  grunt.registerTask('forever-end',
+      'Stops all forever processes running the server.',
+      function() {
+        exec('forever stop ' + grunt.config('serverScript'));
+      });
+
+  grunt.registerMultiTask('validate',
+      'Runs validation checks over all registered file types.',
+      function() {
+        grunt.task.run(this.data);
+      });
+
+  // Load npm tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-concurrent');
-  grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-jsonlint');
-  grunt.loadNpmTasks('grunt-sass');
 
-  grunt.registerTask('static', ['concat', 'uglify', 'cssmin']);
 };
 
