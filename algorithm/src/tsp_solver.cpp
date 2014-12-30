161dfdf2601c37d303a4adee69a08d65adcc1e6e
aicioara <primulandrei@yahoo.com>
2014-12-30 21:49:13 +0200

    Added main methods signatures

---
+++ algorithm/src/tsp_solver.cpp
@@ -0,0 +1,17 @@
+#include "tsp_solver.h"
+
+#include <vector>
+
+void TspSolver::addPoint(int firstNode, int secondNode, int cost) {
+
+}
+void TspSolver::setStartingPoint(int node) {
+
+}
+
+std::vector<int>* TspSolver::solveTsp() {
+	return new std::vector<int>();
+}
+std::vector<int>* TspSolver::solveTspWithGreedy() {
+	return new std::vector<int>();
+}
--- algorithm/src/tsp_solver.h
+++ algorithm/src/tsp_solver.h
@@ -1,4 +1,19 @@
 #ifndef TSP_SOLVER_H
 #define TSP_SOLVER_H
 
+#include <vector>
+
+
+class TspSolver {
+public:
+	virtual ~TspSolver() {};
+
+	void addPoint(int firstNode, int secondNode, int cost);
+	void setStartingPoint(int node);
+
+	std::vector<int>* solveTsp();
+
+	std::vector<int>* solveTspWithGreedy();
+};
+
 #endif /* TSP_SOLVER_H */
