0c6b57ae84db6247a968c5eec6bc34e260d90fe1
aicioara <primulandrei@yahoo.com>
2014-12-30 22:46:15 +0200

    Added methods for greedy and backtracking

--- algorithm/src/tsp_solver.cpp
+++ algorithm/src/tsp_solver.cpp
@@ -2,16 +2,67 @@
 
 #include <vector>
 
-void TspSolver::addPoint(int firstNode, int secondNode, int cost) {
 
+void TspSolver::setNumberOfNodes(int nodes) {
+	adjacencyMatrix = new int*[nodes];
+	for (int i = 0; i < nodes; i++) {
+		adjacencyMatrix[i] = new int[nodes];
+	}
 }
-void TspSolver::setStartingPoint(int node) {
 
+void TspSolver::addPoint(int firstNode, int secondNode, int cost) {
+	if (! init) {
+		throw "The number of nodes in the graph has not been initialized!";
+	}
+
+	adjacencyMatrix[firstNode][secondNode] = cost;
+}
+
+void TspSolver::setStartingPoint(int node) {
+	startingPoint = node;
 }
 
 std::vector<int>* TspSolver::solveTsp() {
-	return new std::vector<int>();
+	if (startingPoint == -1) {
+		throw "Starting point not set";
+	}
+
+	return solveTspWithGreedy();
 }
+
 std::vector<int>* TspSolver::solveTspWithGreedy() {
-	return new std::vector<int>();
+
+	if (startingPoint == -1) {
+		throw "Starting point not set";
+	}
+
+	std::vector<int>* result = new std::vector<int>();
+
+	return result;
+}
+
+std::vector<int>* TspSolver::solveTspWithBacktracking() {
+	throw "TODO";
+
+	if (startingPoint == -1) {
+		throw "Starting point not set";
+	}
+
+	std::vector<int>* result = new std::vector<int>();
+
+	return result;
+}
+
+std::vector<int>* TspSolver::solveTspWithGeneticAlgorithm() {
+
+	throw "TODO";
+
+	if (startingPoint == -1) {
+		throw "Starting point not set";
+	}
+
+
+	std::vector<int>* result = new std::vector<int>();
+
+	return result;
 }
--- algorithm/src/tsp_solver.h
+++ algorithm/src/tsp_solver.h
@@ -8,12 +8,21 @@ class TspSolver {
 public:
 	virtual ~TspSolver() {};
 
+	void setNumberOfNodes(int nodes);
 	void addPoint(int firstNode, int secondNode, int cost);
 	void setStartingPoint(int node);
 
 	std::vector<int>* solveTsp();
 
 	std::vector<int>* solveTspWithGreedy();
+	std::vector<int>* solveTspWithBacktracking();
+	std::vector<int>* solveTspWithGeneticAlgorithm();
+
+private:
+	int** adjacencyMatrix;
+	int startingPoint = -1;
+	bool init = false;
+
 };
 
 #endif /* TSP_SOLVER_H */
