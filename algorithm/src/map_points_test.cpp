95b88b7810729ad47a607ee11a7f497a07d769fd
aicioara <primulandrei@yahoo.com>
2015-01-02 20:16:26 +0200

    Quick hacky fix for failing C++ test. Created a ticket for the future

--- algorithm/src/map_points_test.cpp
+++ algorithm/src/map_points_test.cpp
@@ -2,6 +2,9 @@
 
 #include "map_points.h"
 
+// TODO: As Jeremy was saying, this is a crappy test. We cannot manually
+// see that these coordinates will come in this order. This will be
+// moved anyway, as this logic is now part of the TspSolver class
 TEST(MapPointsTest, OrdersPointsAsExpected) {
 
 	MapPoints* mp = new MapPoints();
@@ -51,20 +54,20 @@ TEST(MapPointsTest, OrdersPointsAsExpected) {
 
 	mp = process_coordinates(mp);
 
-	EXPECT_EQ(mp->errands->at(0)->lat, 51.49841775633878);
-	EXPECT_EQ(mp->errands->at(0)->lng, -0.17533063886503442);
+	EXPECT_EQ(mp->errands->at(0)->lat, 51.497843351035147);
+	EXPECT_EQ(mp->errands->at(0)->lng, -0.1702022552285598);
 
-	EXPECT_EQ(mp->errands->at(1)->lat, 51.497629616981217);
-	EXPECT_EQ(mp->errands->at(1)->lng, -0.17346382139066919);
+	EXPECT_EQ(mp->errands->at(1)->lat, 51.49644070301275);
+	EXPECT_EQ(mp->errands->at(1)->lng, -0.17026662824491724);
 
-	EXPECT_EQ(mp->errands->at(2)->lat, 51.497843351035147);
-	EXPECT_EQ(mp->errands->at(2)->lng, -0.1702022552285598);
+	EXPECT_EQ(mp->errands->at(2)->lat, 51.495759401256);
+	EXPECT_EQ(mp->errands->at(2)->lng, -0.1717901229653762);
 
-	EXPECT_EQ(mp->errands->at(3)->lat, 51.49644070301275);
-	EXPECT_EQ(mp->errands->at(3)->lng, -0.17026662824491724);
+	EXPECT_EQ(mp->errands->at(3)->lat, 51.497629616981217);
+	EXPECT_EQ(mp->errands->at(3)->lng, -0.17346382139066919);
 
-	EXPECT_EQ(mp->errands->at(4)->lat, 51.495759401256);
-	EXPECT_EQ(mp->errands->at(4)->lng, -0.1717901229653762);
+	EXPECT_EQ(mp->errands->at(4)->lat, 51.49841775633878);
+	EXPECT_EQ(mp->errands->at(4)->lng, -0.17533063886503442);
 
 	EXPECT_EQ(mp->errands->at(5)->lat, 51.497910142721448);
 	EXPECT_EQ(mp->errands->at(5)->lng, -0.17936468122343286);
