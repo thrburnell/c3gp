53ba9ddb90eac5bf95a8d4c4ed658940260bd1ec
aicioara <primulandrei@yahoo.com>
2015-01-12 02:02:49 +0000

    Skelleton of the GTSP class

---
+++ algorithm/src/gtsp_solver.cpp
@@ -0,0 +1,115 @@
+#include "gtsp_solver.h"
+
+#include <algorithm>
+#include <limits>
+#include <stdexcept>
+#include <unordered_set>
+#include <vector>
+
+void GtspSolver::setNumberOfNodes(int nodes) {
+    totalNodes = nodes;
+    adjacencyMatrix = new double*[nodes];
+    for (int i = 0; i < nodes; i++) {
+        adjacencyMatrix[i] = new double[nodes];
+    }
+}
+
+void GtspSolver::addPoint(int firstNode, int secondNode, double cost) {
+    if (totalNodes < 0) {
+        throw std::runtime_error("Number of nodes in graph not initialized!");
+    }
+
+    if (firstNode >= totalNodes ||
+        secondNode >= totalNodes ||
+        firstNode < 0 ||
+        secondNode < 0) {
+        throw std::out_of_range("Node indices out of range!");
+    }
+
+    adjacencyMatrix[firstNode][secondNode] = cost;
+}
+
+void GtspSolver::setStartingPoint(int node) {
+    startingPoint = node;
+}
+
+std::vector<int>* GtspSolver::solveGtsp() {
+    checkBuildReady();
+
+    return solveGtspWithBacktracking();
+}
+
+
+// CODE FOR BACKTRACKING
+// TODO: Move this into a separate file or something. Ask someone??
+
+static std::vector<int> tbOrderedResult;
+static std::vector<int> tbAccumulator;
+static double tbCurrent;
+static double tbMinimum;
+static int tbTotalVisited;
+static int* tbVisited;
+
+void GtspSolver::tbBkt(int currNode) {
+
+    tbTotalVisited ++;
+    tbAccumulator.push_back(currNode);
+    tbVisited[currNode] = 1;
+
+    if (tbTotalVisited == totalNodes) {
+        if (tbCurrent + adjacencyMatrix[currNode][startingPoint] < tbMinimum) {
+            tbMinimum = tbCurrent + adjacencyMatrix[currNode][startingPoint];
+            tbOrderedResult.clear();
+            for (const auto& it : tbAccumulator) {
+                tbOrderedResult.push_back(it);
+            }
+        }
+    }
+
+    for (int i = 0; i < totalNodes; i++) {
+        if (! tbVisited[i]) {
+            tbCurrent += adjacencyMatrix[currNode][i];
+            tbBkt(i);
+            tbCurrent -= adjacencyMatrix[currNode][i];
+        }
+    }
+
+    tbVisited[currNode] = 0;
+    tbAccumulator.pop_back();
+    tbTotalVisited --;
+}
+
+std::vector<int>* GtspSolver::solveGtspWithBacktracking() {
+
+    checkBuildReady();
+
+    tbMinimum = std::numeric_limits<double>::max();
+    tbOrderedResult.clear();
+    tbAccumulator.clear();
+    tbVisited = new int[totalNodes];
+    for (int i = 0; i < totalNodes; i++) {
+        tbVisited[i] = 0;
+    }
+    tbCurrent = 0;
+    tbTotalVisited = 0;
+
+    tbBkt(startingPoint);
+
+    return &tbOrderedResult;
+}
+
+
+//GENETIC ALGORITHM CODE
+// TODO: inspired from here
+// https://github.com/parano/GeneticAlgorithm-TSP/blob/master/src/algorithm.js
+
+std::vector<int>* GtspSolver::solveGtspWithGeneticAlgorithm() {
+
+    throw std::runtime_error("TODO solve TSP using Genetic Algorithms");
+
+    checkBuildReady();
+
+    std::vector<int>* result = new std::vector<int>();
+
+    return result;
+}
---
+++ algorithm/src/gtsp_solver.h
@@ -0,0 +1,49 @@
+#ifndef GTSP_SOLVER_H
+#define GTSP_SOLVER_H
+
+#include <stdexcept>
+#include <vector>
+
+/**
+ * Factory-ish pattern implemented here. The way you use this class is
+ *  - Set the number of nodes on which you want to apply the TSP
+ *  - add the points using addPoint method meaning there is a cost
+ *      cost between fromNode to toNode
+ *  - set the starting point (as an integer). Generally it will be node 0
+ *  - call solveTsp.
+ *  - The result is an ordered array of points, where the node on the
+ *      first position (0) will be the starting point for the TSP
+ */
+class GtspSolver {
+
+public:
+    virtual ~GtspSolver() {};
+
+    void setNumberOfNodes(int nodes);
+    void addPoint(int fromNode, int toNode, double cost);
+    void setStartingPoint(int node);
+
+    std::vector<int>* solveGtsp();
+
+    std::vector<int>* solveGtspWithBacktracking();
+    std::vector<int>* solveGtspWithGeneticAlgorithm();
+
+private:
+    double** adjacencyMatrix;
+    int totalNodes = -1;
+    int startingPoint = -1;
+
+    void tbBkt(int currNode);
+
+    void checkBuildReady() {
+        if (startingPoint == -1) {
+            throw std::runtime_error("Starting point not set");
+        } else if (startingPoint >= totalNodes) {
+            throw std::runtime_error(
+                "Starting point greater than no. of nodes");
+        }
+    }
+
+};
+
+#endif /* GTSP_SOLVER_H */
